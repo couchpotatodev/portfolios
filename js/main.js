6127730c9f4b1d1503f6376e11097d9d629d976d
couchpotatodev <mjrimayaho@gmail.com>
2021-04-23 17:36:38 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "../portfolios/week1/index.html"  
+    } /* ,
+{
+    label: "Week2 notes",
+    url: "../wk1/week2/index.html"  
+} */
+
+  ]
+
+var olist = document.getElementById("list");
+
+
+
+for (let i=0; i < links.length; i++){
+    let a = document.createElement("a");
+    let li = document.createElement("li");
+    a.textContent = links[i].label;
+    a.setAttribute('href', links[i].url);
+    li.appendChild(a);
+    olist.appendChild(li); 
+    console.log(olist);
+}
+
+
+
+
+
+
+ 
---
+++ week1/exercise.js
@@ -0,0 +1,21 @@
+function loadStory(){
+     var storyName = document.getElementById('name_input').value;
+     var storyHTML = localStorage.getItem(storyName);
+     document.getElementById('story_editor').value = storyHTML;
+    console.log(storyHTML);
+    }
+
+function saveStory(){
+    var storyName = document.getElementById('name_input').value;
+    var storyHTML = document.getElementById('story_editor').value;
+    localStorage.setItem(storyName, storyHTML);
+    console.log(storyName);
+    console.log(storyHTML);
+    }
+
+function displayStory(){
+   var storyHTML = document.getElementById('story_editor').value;
+   //document.getElementById('story_editor').innerHTML = storyHTML;
+   document.getElementById('story_display').innerHTML = storyHTML;
+   console.log(storyHTML);    
+}
