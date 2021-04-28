97c508d040dc82a84d4008de6076ee843f940cbe
couchpotatodev <mjrimayaho@gmail.com>
2021-04-28 20:38:10 +0800

    edited main

--- week2/js/main.js
+++ week2/js/main.js
@@ -4,3 +4,22 @@ const question = "What is Superman's real name?"
 const answer = prompt(question);
 alert(`You answered ${answer}`);
 
+const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+    ];
+
+let score = 0;
+
+for(const [question,answer] of quiz){
+    const response = prompt(question);
+    if(response === answer){
+    alert('Correct!');
+    score++;
+    } else {
+    alert(`Wrong! The correct answer was ${answer}`);
+    }
+    }
+
+alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
